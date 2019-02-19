5fe4f44970806305cc9382508dd15d1e5c1d7d7b
Nikolaj André Jensen <nikje94@gmail.com>
2019-02-19 14:18:56 +0100

    Dont know

--- DCSNetCore/DoorControlSystem.Test/DoorControlSystem.Test.cs
+++ DCSNetCore/DoorControlSystem.Test/DoorControlSystem.Test.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 
-namespace DoorControlSystem.Unit.Test
+namespace DoorControlSystem.Test
 {
     [TestFixture]
     public class DoorControlUnitTest
